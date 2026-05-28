a90b646031b25396d49a75d1037708a12c5ad73c
Mohitkhatak <khatakmdn143@gmail.com>
2026-05-28 14:58:03 +0530

    Added variable.java

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+         int count = 14;
+         String age = "mohit";
+        boolean isPassed=true;
+        byte marks=57;
+        System.out.println(marks);        
+        System.out.println(age);        
+        System.out.println(count);        
+        System.out.println(isPassed);        
+
+
+    }
+}
