7864264000c87a90daffd7256971740ca8463786
Mohitkhatak <khatakmdn143@gmail.com>
2026-05-29 17:00:03 +0530

    get to know about the operators

---
+++ operators.java
@@ -0,0 +1,17 @@
+public class operators {
+    public static void main(String[] args) {
+        int a= 60;
+        System.out.println(a);
+        int b=7;
+        System.out.println(b);
+        int addition = a+b;
+        int substraction = a-b;
+        int multiplicstion = a*b;
+        int division = a%b;
+        System.out.println(addition + " = addition");
+        System.out.println(substraction + " = substraction");
+        System.out.println(multiplicstion + " = multiplication");
+        System.out.println(division + " = division");
+    }
+    
+}
