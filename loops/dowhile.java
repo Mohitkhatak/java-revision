cd503cbdd1eb0063693a4fbf54ec4f4a5c7f97ba
Mohitkhatak <khatakmdn143@gmail.com>
2026-05-30 17:28:22 +0530

    do while loops

---
+++ loops/dowhile.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class dowhile {
+    public static void main(String[] args) {
+        int i=8;
+
+        do {
+            System.out.println(i);
+            i++;
+            
+        } while (i<=5);
+            System.out.println("out of the loops");
+        
+    }
+}
