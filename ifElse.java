ebc7ed3b4beb00f303808a777928f2f621d447a2
Mohitkhatak <khatakmdn143@gmail.com>
2026-05-30 08:42:27 +0530

    get to know about how Ternary operator   works

--- ifElse.java
+++ ifElse.java
@@ -2,18 +2,23 @@ public class ifElse {
     public static void main(String[] args) {
         int a=32;
         int b=7;
-        int c=9;
-
-        if(a>b){
-            System.out.println("a is bigger than b");
-            if (a>c) {
-                System.out.println("a is also bigger than c");
+        int c=95;
+        int max=0;
+        // if(a>b){
+        //     System.out.println("a is bigger than b");
+        //     if (a>c) {
+        //         System.out.println("a is also bigger than c");
                 
-            }
-            else
-                System.out.println("c is bigger than a");
-        }
-        else 
-            System.out.println("b is bigger than a");
+        //     }
+        //     else
+        //         System.out.println("c is bigger than a");
+        // }
+        // else 
+        //     System.out.println("b is bigger than a");
+
+        max=a > b ? a > c ? a : c : b > c ? b : c;
+
+        System.out.println("value of max"+ max);
+        
     }
 }
