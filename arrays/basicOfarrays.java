f69735d0c30d2cf74a4749801b874e43518c7d08
Mohitkhatak <khatakmdn143@gmail.com>
2026-05-31 11:10:43 +0530

    array declaration and allocation

---
+++ arrays/basicOfarrays.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class basicOfarrays {
+    public static void main(String[] args) {
+    //     int age[];
+    //     age = new int[20]; 
+    // System.out.print(age);
+    // int age[]= new int[5];
+     
+
+    // age[0]=9;
+    // age[1]=7;
+    // age[2]=5;
+    // System.out.println(age[2]);
+    // System.out.println(age[1]);
+    // System.out.println(age[0]);
+    // System.out.println(age.length);
+
+
+    int age[]={5,78,94,67,25,34,749,65,5,58};
+    
+    System.out.println(age[5]);
+    System.out.println(age[9]);
+    System.out.println(age.length);
+
+    }
+}       
