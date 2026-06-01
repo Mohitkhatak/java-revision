331be9d1f83b52d77999a298f93a04e75b8b6b1d
Mohitkhatak <khatakmdn143@gmail.com>
2026-06-01 18:49:31 +0530

    math-function in java

---
+++ methodOrFunctions/learnMath.java
@@ -0,0 +1,29 @@
+package methodOrFunctions;
+
+public class learnMath {
+    public static void main(String[] args) {
+        int a=3 ;
+    
+        int b=-9;
+
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.abs(b));
+        // 3 power  8
+        System.out.println(Math.pow(3,8));
+        // use to get random number bw 1 to 7
+        System.out.println(randomNumber());
+        System.out.println(Math.floor(5.7));
+        System.out.println(Math.ceil(7.8));
+        System.out.println(Math.round(7.8));
+        System.out.println(Math.round(7.4));
+
+
+       
+
+    }
+     public static double randomNumber(){
+        return Math.random()*7+1;
+            
+        }
+}
