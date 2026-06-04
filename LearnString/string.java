612d5835cd529fff4b9632ec2cecb987d104f583
Mohitkhatak <khatakmdn143@gmail.com>
2026-06-04 20:49:20 +0530

    get to know about equals case

--- LearnString/string.java
+++ LearnString/string.java
@@ -4,8 +4,9 @@ public class string {
     public static void main(String[] args) {
         // simple way 
         String name="Mohit";
+        String samename= "Mohit";
 
-        // using new 
+        // using new keyword
         String newname= new String("Mohit khatak");
 
         System.out.println(name);
@@ -14,6 +15,28 @@ public class string {
         // direct  way
 
         System.out.println("Mohit khatak");
+
+        // if (name==samename) {
+        //     System.out.println("both string are same"); 
+        // }
+
+
+        // if (name==newname) {
+        //     System.out.println("both string are same");
+            
+        // }
+        // else{
+        //     System.out.println("both stirng are not same");
+        // }.
+
+
+        // Equals case
+
+        if(name.equals(samename)){
+            System.out.println("new and new name has same values");
+        }else{
+            System.out.println("name and new name are not same");
+        }
 
     }
 }
