47ca4589957271fad90f98f060a64f22dbf5fe90
Mohitkhatak <khatakmdn143@gmail.com>
2026-06-08 16:11:18 +0530

    get to know about the Class and Objects

--- oops/mainclass.java
+++ oops/mainclass.java
@@ -2,14 +2,18 @@ package oops;
 
 public class mainclass {
     public static void main(String[] args) {
-        dog d1=new dog();
-        d1.name="henry";
-        d1.bark();
-        // d1.walk();
+        // dog d1=new dog();
+        // d1.name="henry";
+        // d1.bark();
+        // // d1.walk();
 
-        dog d2=new dog();
-        d2.name="leo";
-        d2.walk();
+        // dog d2=new dog();
+        // d2.name="leo";
+        // d2.walk();
+        Complex num1 = new Complex();
+        num1.a=5;
+        num1.b=58;
+        num1.print();
     }
     
 }
@@ -26,4 +30,12 @@ class dog{
         System.out.println(name+" is barking");
     }
 
+}
+
+class Complex {
+    int a,b;
+     
+     void print(){
+        System.out.println("the complex number is " +a+"+"+b+"i");
+     }
 }
