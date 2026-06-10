e87366d7e2ab974af6d1ce6c768e38ad17a30272
Mohitkhatak <khatakmdn143@gmail.com>
2026-06-10 16:49:23 +0530

    Inheritance

---
+++ oops2/car.java
@@ -0,0 +1,12 @@
+package oops2;
+
+public class car extends vehicle {
+    public static void main(String[] args) {
+        car obj = new car();
+        obj.countwheels=5;
+        obj.start();
+        obj.name="AUDI234";
+        obj.model="4847audi";
+
+    }
+}
---
+++ oops2/vehicle.java
@@ -0,0 +1,12 @@
+package oops2;
+
+public class vehicle {
+    int countwheels;
+    String name;
+
+    String model;
+
+    void start(){
+        System.out.println("vehicle is start");
+    }
+}
