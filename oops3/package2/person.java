9f6d55f7b2d9a04f2eab7f661d54c9cc2386ede7
Mohitkhatak <khatakmdn143@gmail.com>
2026-06-12 16:59:46 +0530

    Abstract class

--- oops3/package2/person.java
+++ oops3/package2/person.java
@@ -3,22 +3,21 @@ package oops3.package2;
 public class person {
     private int age;
     private String name;
-    boolean canbeChange=true;
+    boolean canbeChange = true;
 
-    public void setAge(int age){
-        if (canbeChange){
-            if (age>0) {
-                 this.age =age;
-                
+    public void setAge(int age) {
+        if (canbeChange) {
+            if (age > 0) {
+                this.age = age;
+
             }
 
         }
-       
-        
+
     }
 
-    public int  getAge(){
-        return age; 
+    public int getAge() {
+        return age;
     }
-    
+
 }
---
+++ oops4/LearnAbstract.java
@@ -0,0 +1,38 @@
+package oops4;
+
+public class LearnAbstract {
+    public static void main(String[] args) {
+        // vehicle veh=new vehicle(); we cannot make objects of an abstract class but we
+        // can make method
+        car c1= new car();
+        c1.accelrate();
+        c1.breakes(8);
+
+    }
+
+}
+
+abstract class vehicle {
+    abstract void accelrate();
+
+    abstract int breakes(int wheels); // abstract class methods doesnt have body
+
+} 
+class car extends vehicle{
+
+    @Override
+    void accelrate() {
+        System.out.println("car is accelrate");
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    int breakes( int wheels) {
+        System.out.println("car breakes pushed");
+        return wheels;
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
